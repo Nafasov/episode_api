1f7d81e8b47216f08044ddee0793aaf335673f1b
Nafasov <nafasovnurxon18@gmail.com>
2024-03-27 08:56:20 +0500

    modified

---
+++ apps/episode/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+
+app_name = 'episode'
+
+urlpatterns = [
+
+]
+
+
+"""
+
+    Tag
+    -list
+    
+    Category
+    -list
+    
+    Episode
+    -list
+    -detail
+    -create
+    -update
+    -delete
+    
+    Comment
+    -list
+    -create
+    -delete
+    
+    EpisodeLike
+    -like
+    -dislike
+    
+"""
