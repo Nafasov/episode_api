0824d43089f4c220834e5b4f104546736624239a
Nafasov <nafasovnurxon18@gmail.com>
2024-03-29 13:12:52 +0500

    modified

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -32,7 +31,7 @@ schema_view = get_schema_view(
       license=openapi.License(name="BSD License"),
    ),
    public=True,
-   permission_classes=(permissions.AllowAny,),
+   permission_classes=(permissions.AllowAny, ),
 )
 
 
@@ -42,5 +41,6 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path('account/', include('apps.account.urls', namespace='account'))
+    path('account/', include('apps.account.urls', namespace='account')),
+    path('episode/', include('apps.episode.urls', namespace='episode'))
 ]
